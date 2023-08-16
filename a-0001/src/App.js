0d324ea2810efc0ef2f951d5c854039f71cacc0b
KellmeSouza <kellmesouza@gmail.com>
2023-08-16 01:19:43 -0300

    ..

--- a-0001/src/App.js
+++ a-0001/src/App.js
@@ -2,9 +2,24 @@
 import './App.css';
 
 function App() {
-  return (<div>
-   <p>Oláa</p>
-   <p>azul</p>
+  return (
+    /* começo do corpo do codigo */
+     /* lembrando essa é a estrutura HTML da pagina adaptada para jsx,
+     a app.css é estilo e o index.js estenções de js e o index.css estilo das estenções js */
+  <div id="corpo" className="corpo">
+     {/* lembrando essa é a estrutura HTML da pagina adaptada para jsx */}
+    {/* barra de menu do codigo respansivo ao maximo */}
+   <selection id="menu" className="menu"></selection>
+    {/* lembrando essa é a estrutura HTML da pagina adaptada para jsx */}
+   {/* carrocel para que mostre as imagens iniciais deixando interativo */}
+   <div id="carrocel" classname="carrocel"></div>
+    {/* lembrando essa é a estrutura HTML da pagina adaptada para jsx */}
+   {/* conteudo da pagina */}
+   <div id="conteudo" classname="conteudo"></div>
+    {/* lembrando essa é a estrutura HTML da pagina adaptada para jsx */}
+   {/* rodape da pagina */}
+   <selection id="rodape" className="rodape"></selection>
+    {/* lembrando essa é a estrutura HTML da pagina adaptada para jsx */}
    </div>
    );
 }
